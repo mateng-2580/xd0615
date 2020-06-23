655863749090e9c1c0b0128e3771dbe368d3c1bb
mateng-2580 <2356679211@qq.com>
2020-06-23 17:51:33 +0800

    第七次作业by马腾19140200212

--- lesson7/code1.c
+++ lesson7/code1.c
@@ -1,36 +1,41 @@
-#include <MsTimer2.h>     //定时器库的头文件
- 
-int tick = 0; //计数值
- 
-//中断服务程序
-byte income=0;
-void onTimer()
+
+volatile int pinInterrupt = 2; //接中断信号的脚
+volatile byte i=0; 
+volatile byte income=0;  
+void myfunc()
 {
-   int i;
-    {for(i=0;i<10;i++)
-    {
-    income=i;
-    income=income-'0';
-    digitalWrite(2,income&0x1);
-    digitalWrite(3,(income>>1)&0x1);
-    digitalWrite(4,(income>>2)&0x1);
-    digitalWrite(5,(income>>3)&0x1);
-    delay(1000);
-    }}
+   if ( digitalRead(pinInterrupt) == LOW )
+     Serial.println("Key UP");
+   else
+     Serial.println("Key UP");
 }
  
 void setup()
 {
-  Serial.begin(9600); //初始化串口
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  MsTimer2::set(1000, onTimer); //设置中断，每1000ms进入一次中断服务程序 onTimer()
-  MsTimer2::start(); //开始计时
+   Serial.begin(9600); //打开串口
+   pinMode( pinInterrupt, INPUT);//设置管脚为输入
+   //Enable中断管脚, 中断服务程序为onChange(), 监视引脚变化
+   attachInterrupt( digitalPinToInterrupt(pinInterrupt), myfunc, CHANGE);
+   pinMode(6, OUTPUT);
+   pinMode(3, OUTPUT);
+   pinMode(4, OUTPUT);
+   pinMode(5, OUTPUT);
 }
+
  
 void loop()
 {
-  
+   {for(i=0;i<10;i++)
+    {
+    income=i;
+    income=income-'0';
+    digitalWrite(6,income&0x1);
+    digitalWrite(3,(income>>1)&0x1);
+    digitalWrite(4,(income>>2)&0x1);
+    digitalWrite(5,(income>>3)&0x1);
+     delay(1000);
+    }}
+ 
 }
+
+
